3bf7e62791c13ec90df3dade4dfde3132e504c83
claudiofernandesdev <claudiofernandes.dev@mail.com>
2023-01-12 21:26:18 -0300

    update and delet routes add

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ const connectToDb = () => {
         "mongodb+srv://root:Claudio1412@todolist.phrx6j6.mongodb.net/?retryWrites=true&w=majority",
         {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
+            useUnifiedTopology: true,            
         }
     ).then(() => console.log("MongoDB Atlas Conectado!"))
     .catch((err) => console.log(err));
